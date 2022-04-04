a049affdf1702b4ba9d2399713d7c2e152998bb8
Ting-Yat Wong <tywong@modv-ve703-02575.apn.wlan.private.upenn.edu>
2022-04-04 11:02:30 -0400

    [fixed] variable name

--- scripts/03_data_visualization.R
+++ scripts/03_data_visualization.R
@@ -23,7 +23,7 @@ validation_df <- read_rds(here("data", "processed", "holdout_dataset.rds"))
 
 full_df <- discovery_df %>% 
     mutate(split = "Discovery", .before = Age) %>% 
-    bind_rows(validation_df %>% mutate(split = "Validation", .before = Age))
+    bind_rows(validation_df %>% mutate(split = "Holdout", .before = Age))
 
 
 # Distribution Visualization --------------------------------------------------
@@ -93,7 +93,7 @@ discovery_comparison_table <- discovery_df %>%
     add_q() %>% 
     bold_p(q = TRUE)
 
-validation_comparison_table <- validation_df %>% 
+holdout_comparison_table <- validation_df %>% 
     mutate(psy_rank = ntile(g_psy, 4)) %>% 
     filter(psy_rank %in% c(1, 4)) %>% 
     mutate(psy_rank = factor(
@@ -130,8 +130,8 @@ validation_comparison_table <- validation_df %>%
     bold_p(q = TRUE)
 
 merged_comparison_table <- tbl_merge(
-    tbls = list(discovery_comparison_table, validation_comparison_table),
-    tab_spanner = c("**Discovery Dataset**", "**Validation Dataset**"))
+    tbls = list(discovery_comparison_table, holdout_comparison_table),
+    tab_spanner = c("**Discovery Dataset**", "**Holdout Dataset**"))
 
 merged_comparison_table %>% 
     as_gt() %>% 
@@ -146,32 +146,23 @@ merged_comparison_table %>%
     )
 
 
-
-
-
-
-
-
-
-
-# 
-corr_res <- full_df %>% 
-    select(bpaq_tot:bpaq_host, all_of(psychopathology_list)) %>% 
-    psych::corr.test()
-
+# Visualization Partial Correlation --------------------------------------------
+psychopathology_label <- c("DEP", "Mania", "HM", "GAD", "LSAS", "OCD", "CAPE",
+                           "PQB")
 
 network_data <- list()
-network_data$data <- preprocessed_df %>% select(bpaq_phy:bpaq_host, all_of(psychopathology_list)) 
+network_data$data <- full_df %>% select(bpaq_phy:bpaq_host, all_of(psychopathology_list)) 
 network_data$node_col <- c(rep("#B5CAA0", 4), rep("#DAC9A6", 8))
 network_data$lable <- c("Phy", "Verb", "Anger", "Host", psychopathology_label)
 network_data$group <- c("1. Outcomes", rep("2. Features", 8))
 
-
+library(bootnet)
+library(qgraph)
 g <- estimateNetwork(
     network_data$data,
     default = "EBICglasso",
     threshold = TRUE
 )
 
-g$graph %>% qgraph(layout = "spring", labels = network_data$lable, color = network_data$node_col)
+qgraph(g$graph, layout = "spring", labels = network_data$lable)
 
