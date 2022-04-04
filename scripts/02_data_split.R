1fb9a10b170e2d0466cd0e230f0271585656b653
Ting-Yat Wong <tywong@modv-ve703-02575.apn.wlan.private.upenn.edu>
2022-04-04 09:46:55 -0400

    [add] data split to discovery and validation

--- scripts/02_data_split.R
+++ scripts/02_data_split.R
@@ -12,10 +12,10 @@ preprocessed_df <- here("data", "processed", "yes_baseline_outcome-aggression_n-
 # Split for Exploration and Holdout Datasets -----------------------------------
 set.seed(1234)
 splits <- initial_split(preprocessed_df, prop = 3/4, stata = c(bpaq_tot, g_psy))
-exploration_df <- training(splits)
+discovery_df <- training(splits)
 holdout_df <- testing(splits)
 
-write_rds(exploration_df, here("data", "processed", "exploration_dataset.rds"))
+write_rds(discovery_df, here("data", "processed", "discovery_dataset.rds"))
 write_rds(holdout_df , here("data", "processed", "holdout_dataset.rds"))
 
 
