9a77392a6d424f470f8b157f9829d32ae82b83bd
Ting-Yat Wong <yatyat0321@gmail.com>
2022-10-28 16:54:09 -0400

    [new] add new network analyses

---
+++ scripts/07_network_overlappedfeatures_outcomes.R
@@ -0,0 +1,283 @@
+# Environment ------------------------------------------------------------------
+library(tidyverse)
+library(here)
+library(mgm)
+library(qgraph)
+library(ggpubr)
+library(ggthemes)
+library(bootnet)
+library(pheatmap)
+library(RColorBrewer)
+library(gtsummary)
+
+source(here("src", "R", "utilities.R"))
+
+# Data IO  ---------------------------------------------------------------------
+overlapping_features <- read_rds(
+    here("data", "processed", "overlapping_features.rds")
+)
+
+fa_feature_details <- 
+    here("data", "processed", "fa_features_details.csv") %>% 
+    read_csv(show_col_types = FALSE)
+
+n_features <- length(fa_feature_details$label)
+print(glue::glue("A total of {n_features} is selected"))
+
+#discovery_df <-  here("data", "processed", "discovery_dataset.rds") %>% 
+#    read_rds() %>% 
+#    select(bpaq_scaled, g_psy, all_of(overlapping_top_features)) %>% 
+#    drop_na()
+
+discovery_df <- read_rds(here("data", "processed", "discovery_dataset.rds"))
+holdout_df <- read_rds(here("data", "processed", "holdout_dataset.rds"))
+
+full_df <- discovery_df %>% 
+    mutate(split = "Discovery", .before = Age) %>% 
+    bind_rows(holdout_df %>% mutate(split = "Holdout", .before = Age)) %>% 
+    select(bpaq_scaled, g_psy, all_of(overlapping_features)) %>% 
+    drop_na() %>% 
+    mutate(type = "No Missing")
+
+# compare drop out and no drop out samples
+comparison_df <- discovery_df %>% 
+    mutate(split = "Discovery", .before = Age) %>% 
+    bind_rows(holdout_df %>% mutate(split = "Holdout", .before = Age)) %>% 
+    mutate(type = "Original") %>% 
+    bind_rows(
+        discovery_df %>% 
+            mutate(split = "Discovery", .before = Age) %>% 
+            bind_rows(holdout_df %>% mutate(split = "Holdout", .before = Age)) %>% 
+            drop_na() %>% 
+            mutate(type = "No Missing")
+    )
+
+comparison_df %>% 
+    select(type, Age, Sex, Edu_TotYr, bpaq_tot, g_psy) %>%
+    mutate(type = factor(type, levels = c("Original", "No Missing"))) %>% 
+    tbl_summary(
+        by = type,
+        statistic = list(all_continuous() ~ "{mean} ({sd})"),
+        digits = all_continuous() ~ c(2, 2),
+        label = list(
+            Edu_TotYr = "Years of Education",
+            bpaq_tot = "Trait Aggression",
+            g_psy = "General Psychopathology",
+            cidi_e34 = "Aggressive Behavior (Lifetime)"
+        )
+    ) %>% 
+    add_p() %>% 
+    add_q() %>% 
+    bold_p(q = TRUE) %>% 
+    as_gt() %>% 
+    gt::gtsave(here("outputs", "tables", "compaison-full_vs_dropout.html"))
+
+
+# Dimension Reduction ----------------------------------------------------------
+set.seed(1234)
+parallel <- psych::fa.parallel(
+    x = full_df %>% select(overlapping_features), 
+    fm = "minres", 
+    fa = "fa",
+    n.iter = 100,
+    plot = FALSE
+)
+parallel_fig <- plot_faparallel(parallel)
+parallel_fig
+ggsave(
+    plot = parallel_fig, 
+    filename = here("outputs", "figs", "fa_parallel_res.pdf"),
+    height = 6,
+    width = 12
+)
+
+fa_res <- psych::fa(
+    full_df %>% select(overlapping_features), 
+    nfactors = 18, 
+    rotate = "oblimin", 
+    fm = "minres"
+)
+
+fa_score_df <- full_df %>% 
+    select(g_psy, bpaq_scaled) %>%
+    bind_cols(fa_res$scores) %>% 
+    rename_at(vars(starts_with("MR")), ~str_replace(., "MR", "F"))
+
+# plot FA loading
+pdf(file = here("outputs", "figs", "fa_heatmap.pdf"), width = 12, height = 14)
+pheatmap(
+    fa_res$loadings %>% 
+        unclass() %>% 
+        as_tibble(rownames = NA) %>% 
+        rename_at(vars(starts_with("MR")), ~str_replace(., "MR", "F")) %>% 
+        rownames_to_column() %>% 
+        select(rowname, fa_feature_details$label) %>% 
+        column_to_rownames("rowname"),
+    color = colorRampPalette(c("navy", "white", "tomato3"))(40),
+    breaks = seq(-1, 1, by = 0.05),
+    display_numbers = TRUE,
+    cluster_rows = TRUE,
+    treeheight_row = 0,
+    cluster_cols = FALSE,
+    angle_col = 0,
+    width = 10, 
+    height = 12
+)
+dev.off()
+
+
+# Symptom Network --------------------------------------------------------------
+network_data       <- list()
+network_data$data  <- fa_score_df %>% select(g_psy, bpaq_scaled, fa_feature_details$label)
+network_data$node  <- c("GPsy", "Aggr", fa_feature_details$label)
+network_data$node_detail <- c("General Psychoapthology",
+                              "Trait Aggression",
+                              fa_feature_details$details)
+network_data$node_col <- c("#B5CAA0", "#B5CAA0", rep("#DAC9A6", n_features))
+network_data$group <- c("1. Outcomes", "1. Outcomes", rep("2. Features", n_features))
+
+set.seed(1234)
+g <- estimateNetwork(
+    network_data$data, 
+    default = "ggmModSelect",
+    stepwise = TRUE,
+    corMethod = "cor_auto"
+)
+
+centrality_plot <- g %>%
+    centralityPlot(
+        print   = FALSE,
+        labels  = network_data$node,
+        include = "ExpectedInfluence",
+        orderBy = "ExpectedInfluence"
+    ) +
+    scale_x_continuous(limits = c(-0.3, 1.1)) +
+    labs(x = "Standardized Centrality Indices") +
+    theme(axis.title = element_text(size = 8)) +
+    theme_pander()
+centrality_plot
+ggsave(here("outputs", "figs", "data-discovery_desc-network_centrality.pdf"),
+       centrality_plot, height = 6, width = 4)
+
+# plot the graph
+network_graph <- qgraph(
+    g$graph,
+    # layout
+    layout = "spring",
+    repulsion = 1.1,
+    color = network_data$node_col,
+    labels = network_data$node,
+    groups = network_data$group,
+    nodeNames = network_data$node_detail,
+    # node
+    vsize = 4,
+    vTrans = 250,
+    label.fill.vertical = 0.2,
+    borders = FALSE,
+    # edge
+    esize = 10,
+    fade = TRUE,
+    # edge curvature
+    curve = 0.5,
+    curveAll = TRUE,
+    # legend
+    legend = TRUE, 
+    legend.cex = 0.7,
+    GLratio = 1.5,
+    cut = 0,
+    minimum = 0,
+    theme = "TeamFortress",
+    filetype = "pdf",
+    filename = here("outputs", "figs", "data-discovery_desc-outcomes_features_network")
+)
+
+
+flow_graph <- qgraph(
+    g$graph,
+    # layout
+    layout = "spring",
+    repulsion = 1.1,
+    color = network_data$node_col,
+    labels = network_data$node,
+    groups = network_data$group,
+    #nodeNames = network_data$node_detail,
+    # node
+    vsize = 4,
+    vTrans = 250,
+    label.fill.vertical = 0.2,
+    borders = FALSE,
+    # edge
+    esize = 10,
+    fade = TRUE,
+    # edge curvature
+    curve = 0.5,
+    curveAll = TRUE,
+    # legend
+    legend = FALSE, 
+    cut = 0,
+    minimum = 0,
+    theme = "TeamFortress",
+    mar = c(10, 10, 10, 10)
+)
+flow(
+    flow_graph, 
+    from = 1,
+    filetype = "pdf",
+    filename = here("outputs", "figs", "data-discovery_desc-gpsy_flow_network")
+)
+flow(
+    flow_graph, 
+    from = 2,
+    filetype = "pdf",
+    filename = here("outputs", "figs", "data-discovery_desc-aggr_flow_network")
+)
+
+# Stability --------------------------------------------------------------------
+
+# check stability of centrality
+g_bs_case <- bootnet(
+    g,
+    statistics = c("ExpectedInfluence", "Edge"),
+    nBoots = 1000,
+    nCores = 8,
+    type = "case"
+)
+write_rds(g_bs_case, here("outputs", "cache", "g_bs_case.rds"))
+
+g_bs_np <- bootnet(
+    g,
+    statistics = c("ExpectedInfluence", "Edge"),
+    nBoots = 1000,
+    nCores = 8,
+    type = "nonparametric"
+)
+write_rds(g_bs_np, here("outputs", "cache", "g_bs_np.rds"))
+
+stability_drop_plot <- plot(g_bs_case, statistics = "ExpectedInfluence") + 
+    theme_pander() +
+    theme(
+        legend.position = "none",
+        plot.margin = margin(2, 2, 2, 2, "mm")
+    )
+stability_drop_plot
+ggsave(
+    here("outputs", "figs", "data-discovery_desc-bs_stability_drop.pdf"),
+    stability_plot, 
+    height = 4, 
+    width = 8
+)
+
+stability_edge_plot <- plot(g_bs_np,  statistics = "Edge", order = "sample") +
+    theme_bw() +
+    theme(
+        legend.position = "none",
+        plot.margin = margin(2, 2, 2, 2, "mm")
+    )
+stability_edge_plot
+ggsave(
+    here("outputs", "figs", "data-discovery_desc-bs_stability_edge.pdf"),
+    stability_edge_plot, 
+    height = 18,
+    width = 8
+)
+plot(g_bs_np, plot = "interval", split0 = TRUE, statistics = "Edge", order = "sample")
