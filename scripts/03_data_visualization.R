d68a0bde9d3ffef6c682618d1596c4db7b84715e
Ting-Yat Wong <yatyat0321@gmail.com>
2022-10-28 16:50:08 -0400

    [feat] new hexagon plot and correlation heatmap

--- scripts/03_data_visualization.R
+++ scripts/03_data_visualization.R
@@ -2,8 +2,14 @@
 library(tidyverse)
 library(here)
 library(gtsummary)
+library(ggside)
+library(ggpubr)
 library(flextable)
 library(officer)
+library(ggthemes)
+library(patchwork)
+library(bootnet)
+library(qgraph)
 
 sect_properties_wide <- prop_section(
     page_size = page_size(orient = "landscape",
@@ -17,18 +23,35 @@ sect_properties_wide <- prop_section(
 psychopathology_list <- c("PHQ9_tot", "YMRS_tot", "HCL32_tot", "GAD7_tot",
                           "LSAS_Tot", "YBOCS_Sym", "Cape_distress_tot",
                           "PQB_tot")
+psychopathology_label <- c("Depression", "Mania", "Hypomania", "General Anxiety", 
+                           "Social Anxiety", "Obsessive-Compulsive", 
+                           "Psychotic-like Experiences", "Prodromal Psychosis")
+aggressiveness_list <- c("bpaq_tot", "bpaq_phy", "bpaq_verb", "bpaq_anger", 
+                         "bpaq_host")
+aggressiveness_label <- c("BPAQ total", "BPAQ physical aggression",
+                          "BPAQ verbal aggression", "BPAQ anger",
+                          "BPAQ hostility")
+
 
 discovery_df <- read_rds(here("data", "processed", "discovery_dataset.rds"))
-validation_df <- read_rds(here("data", "processed", "holdout_dataset.rds"))
+holdout_df <- read_rds(here("data", "processed", "holdout_dataset.rds"))
 
 full_df <- discovery_df %>% 
     mutate(split = "Discovery", .before = Age) %>% 
-    bind_rows(validation_df %>% mutate(split = "Holdout", .before = Age))
+    bind_rows(holdout_df %>% mutate(split = "Holdout", .before = Age))
 
 
 # Distribution Visualization --------------------------------------------------
+
+full_df %>% 
+    group_by(Sex) %>% 
+    summarize(
+        mean_age = mean(Age),
+        sd_age = sd(Age)
+    )
+
 full_compaison_table <- full_df %>% 
-    select(split, Age, Sex, Edu_TotYr, cidi_e34, bpaq_tot, g_psy) %>%
+    select(split, Age, Sex, Edu_TotYr, bpaq_tot, g_psy) %>%
     tbl_summary(
         by = split,
         statistic = list(all_continuous() ~ "{mean} ({sd})"),
@@ -58,185 +81,125 @@ full_compaison_table %>%
     )
 
 
-# Table Benign vs. High Psychopathology ---------------------------------------
-discovery_comparison_table <- discovery_df %>% 
-    mutate(cidi_e34 = factor(cidi_e34, levels = c(0, 1), labels = c("No", "Yes"))) %>% 
-    #filter(psy_rank %in% c(1, 4)) %>% 
-    #mutate(psy_rank = factor(
-    #        psy_rank, levels = c(1, 4), 
-    #        labels = c("Low", "High")
-    #   )
-    #) %>% 
-    select(Age, Sex, Edu_TotYr, cidi_e34, bpaq_tot:bpaq_host, g_psy,
-           all_of(psychopathology_list)) %>% 
-    tbl_summary(
-        by = cidi_e34,
-        statistic = list(all_continuous() ~ "{mean} ({sd})"),
-        digits = all_continuous() ~ c(2, 2),
-        label = list(
-            Edu_TotYr = "Years of Education",
-            PHQ9_tot = "Depression (PHQ-9)",
-            YMRS_tot = "Mania (YMRS)",
-            HCL32_tot = "Hypomania (HCL−32)",
-            GAD7_tot = "GAD (GAD−7)",
-            LSAS_Tot = "Social Anxiety (LSAS)",
-            YBOCS_Sym = "OCD (Y−BOCS)",
-            Cape_distress_tot = "PLE (CAPE−P−15)",
-            PQB_tot = "Prodrome (PQB)",
-            bpaq_tot = "BPAQ total (log)",
-            bpaq_phy = "BPAQ physical aggression",
-            bpaq_verb = "BPAQ verbal aggression",
-            bpaq_anger = "BPAQ anger",
-            bpaq_host = "BPAQ hostility",
-            cidi_e34 = "Aggressive Behavior (Lifetime)",
-            g_psy = "General Psychopathology"
-        )
-    ) %>% 
-    add_p() %>% 
-    add_q() %>% 
-    bold_p(q = TRUE)
-
-holdout_comparison_table <- validation_df %>% 
-    mutate(cidi_e34 = factor(cidi_e34, levels = c(0, 1), labels = c("No", "Yes"))) %>% 
-    select(Age, Sex, Edu_TotYr, cidi_e34, bpaq_tot:bpaq_host, g_psy,
-           all_of(psychopathology_list)) %>% 
-    tbl_summary(
-        by = cidi_e34,
-        type = YMRS_tot ~ "continuous",
-        statistic = list(all_continuous() ~ "{mean} ({sd})"),
-        digits = all_continuous() ~ c(2, 2),
-        label = list(
-            Edu_TotYr = "Years of Education",
-            PHQ9_tot = "Depression (PHQ-9)",
-            YMRS_tot = "Mania (YMRS)",
-            HCL32_tot = "Hypomania (HCL−32)",
-            GAD7_tot = "GAD (GAD−7)",
-            LSAS_Tot = "Social Anxiety (LSAS)",
-            YBOCS_Sym = "OCD (Y−BOCS)",
-            Cape_distress_tot = "PLE (CAPE−P−15)",
-            PQB_tot = "Prodrome (PQB)",
-            bpaq_tot = "BPAQ total (log)",
-            bpaq_phy = "BPAQ physical aggression",
-            bpaq_verb = "BPAQ verbal aggression",
-            bpaq_anger = "BPAQ anger",
-            bpaq_host = "BPAQ hostility",
-            cidi_e34 = "Aggressive Behavior (Lifetime)",
-            g_psy = "General Psychopathology"
-        )
-    ) %>% 
-    add_p() %>% 
-    add_q() %>% 
-    bold_p(q = TRUE)
-
-merged_comparison_table <- tbl_merge(
-    tbls = list(discovery_comparison_table, holdout_comparison_table),
-    tab_spanner = c("**Discovery Dataset**", "**Holdout Dataset**"))
-merged_comparison_table
-
-merged_comparison_table %>% 
-    as_gt() %>% 
-    gt::gtsave(here("outputs", "tables", "comparison_begnin_high.html"))
-
-merged_comparison_table %>% 
-    as_flex_table() %>% 
-    bold(part = "header") %>% 
-    save_as_docx(
-        path = here("outputs", "tables", "comparison_begnin_high.docx"),
-        pr_section = sect_properties_wide
-    )
-
-m1_discovery <- discovery_df %>% 
-    glm(formula = cidi_e34 ~ Age + Sex + Edu_TotYr + bpaq_tot + g_psy, family = binomial(link="logit"))
-
-m1_holdout <- validation_df %>% 
-    glm(formula = cidi_e34 ~ Age + Sex + Edu_TotYr + bpaq_tot + g_psy, family = binomial(link="logit"))
-
-m1_discovery_table <- tbl_merge(
-    list(
-        tbl_regression(
-            m1_discovery, 
-            tidy_fun = tidy_standardize,
-            label = list(
-                Edu_TotYr = "Years of Education",
-                bpaq_tot = "Trait Aggression",
-                g_psy = "General Psychopathology"
-            )
-        ) ,
-        tbl_regression(
-            m1_discovery,
-            label = list(
-                Edu_TotYr = "Years of Education",
-                bpaq_tot = "Trait Aggression",
-                g_psy = "General Psychopathology"
-            )
-        ) %>%
-            modify_column_hide(c(estimate, ci)) %>% 
-            bold_p
-    )
-) %>% 
-    modify_spanning_header(c(estimate_1, ci_1, p.value_2) ~ NA)
-
-m1_holdout_table <- tbl_merge(
-    list(
-        tbl_regression(
-            m1_holdout, 
-            tidy_fun = tidy_standardize,
-            label = list(
-                Edu_TotYr = "Years of Education",
-                bpaq_tot = "Trait Aggression",
-                g_psy = "General Psychopathology"
-            )
-        ),
-        tbl_regression(
-            m1_holdout,
-            label = list(
-                Edu_TotYr = "Years of Education",
-                bpaq_tot = "Trait Aggression",
-                g_psy = "General Psychopathology"
-            )
-        ) %>%
-            modify_column_hide(c(estimate, ci)) %>% 
-            bold_p
-    )
-) %>% 
-    modify_spanning_header(c(estimate_1, ci_1, p.value_2) ~ NA)
-
-merged_reg_table <- tbl_merge(
-    tbls = list(m1_discovery_table, m1_holdout_table),
-    tab_spanner = c("**Discovery Dataset**", "**Holdout Dataset**"))
-merged_reg_table
+# Correlation between Trait Aggression and General Psychopathology -------------
+# show the correlations
+full_df %>%
+    select(split, g_psy, bpaq_tot) %>% 
+    group_by(split) %>% 
+    correlation::correlation(method = "pearson", p_adjust = "fdr")
 
 
-merged_reg_table %>% 
-    as_gt() %>% 
-    gt::gtsave(here("outputs", "tables", "reg_aggresivebehavior.html"))
-
-merged_reg_table %>% 
-    as_flex_table() %>% 
-    bold(part = "header") %>% 
-    save_as_docx(
-        path = here("outputs", "tables", "reg_aggresivebehavior.docx"),
-        pr_section = sect_properties_wide
-    )
-
-
-# Visualization Partial Correlation --------------------------------------------
-psychopathology_label <- c("DEP", "Mania", "HM", "GAD", "LSAS", "OCD", "CAPE",
-                           "PQB")
-
-network_data <- list()
-network_data$data <- full_df %>% select(cidi_e34, bpaq_phy:bpaq_host, all_of(psychopathology_list)) 
-network_data$node_col <- c(rep("#B5CAA0", 5), rep("#DAC9A6", 8))
-network_data$lable <- c("AggrBeh", "Phy", "Verb", "Anger", "Host", psychopathology_label)
-network_data$group <- c("1. Outcomes", rep("2. Features", 8))
-
-library(bootnet)
-library(qgraph)
-g <- estimateNetwork(
-    network_data$data,
-    default = "EBICglasso",
-    threshold = TRUE
+hexgonplot_corr <- full_df %>% 
+    ggplot(aes(x = g_psy, y = bpaq_tot)) +
+        geom_hex() +
+        scale_fill_gradient(low = "grey70", high = "grey5") +
+        geom_smooth(method = "lm", color = "tomato3", fill = "tomato3") +
+        stat_cor(method = "pearson", label.x = 6, label.y = 13) +
+        labs(
+            x = "General Psychopathology (a.u.)", 
+            y = "BPAQ Total",
+            fill = "Bin Size"
+        ) +
+        geom_xsidedensity(
+            aes(y = stat(density)),
+            color = NA, 
+            fill = "lightgrey",
+            alpha    = 0.7
+        ) +
+        geom_ysidedensity(
+            aes(x = stat(density)), 
+            color = NA, 
+            fill = "lightgrey",
+            alpha    = 0.7
+        ) +
+        facet_wrap(vars(split)) +
+        guides(fill = guide_colourbar(barwidth = 10, barheight = 1)) +
+        theme_minimal() +
+        theme(
+            legend.position = "bottom",
+            plot.margin = margin(2, 2, 2, 2, "mm"),
+            ggside.panel.scale.x = 0.2,
+            ggside.panel.scale.y = 0.2,
+            text = element_text(size = 24)
+        ) +
+        theme_ggside_void()
+hexgonplot_corr
+        
+ggsave(
+    plot = hexgonplot_corr, 
+    filename = here("outputs", "figs", "hexgonplot_aggressiveness_psychopathology.pdf"),
+    width = 14,
+    height = 8
 )
 
-qgraph(g$graph, layout = "spring", labels = network_data$lable)
+corr_heatmap_discovery <- discovery_df %>% 
+    correlation::correlation(
+        select = c("g_psy", psychopathology_list),
+        select2 = aggressiveness_list,
+        method = "pearson", p_adjust = "fdr"
+    ) %>% 
+    as_tibble() %>% 
+    mutate(r = if_else(p < 0.05, r, 0)) %>% 
+    filter(!str_detect(Parameter1, "bpaq")) %>% 
+    filter(str_detect(Parameter2, "bpaq")) %>% 
+    mutate(Parameter1 = factor(
+        Parameter1, 
+        levels = c("g_psy", psychopathology_list),
+        labels = c("General Psychopathology", psychopathology_label)
+    )
+    ) %>% 
+    mutate(Parameter2 = factor(
+        Parameter2, 
+        levels = rev(aggressiveness_list),
+        labels = rev(aggressiveness_label)
+    )
+    ) %>% 
+    ggplot(aes(x = Parameter1, y = Parameter2, fill = r)) +
+        geom_tile(color = "grey10") +
+        scale_fill_viridis_c(begin = 0.2, limits = c(0.05, 0.65)) +
+        labs(x = "", y = "", title = "Discovery") +
+        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
+corr_heatmap_holdout <- holdout_df %>% 
+    correlation::correlation(
+        select = c("g_psy", psychopathology_list),
+        select2 = aggressiveness_list,
+        method = "pearson", p_adjust = "fdr"
+    ) %>% 
+    as_tibble() %>% 
+    mutate(r = if_else(p < 0.05, r, 0)) %>% 
+    filter(!str_detect(Parameter1, "bpaq")) %>% 
+    filter(str_detect(Parameter2, "bpaq")) %>% 
+    mutate(Parameter1 = factor(
+            Parameter1, 
+            levels = c("g_psy", psychopathology_list),
+            labels = c("General Psychopathology", psychopathology_label)
+        )
+    ) %>% 
+    mutate(Parameter2 = factor(
+            Parameter2, 
+            levels = rev(aggressiveness_list),
+            labels = rev(aggressiveness_label)
+        )
+    ) %>% 
+    ggplot(aes(x = Parameter1, y = Parameter2, fill = r)) +
+        geom_tile(color = "grey10") +
+        scale_fill_viridis_c(begin = 0.2, limits = c(0.05, 0.65)) +
+        labs(x = "", y = "", title = "Holdout") +
+        theme(
+            axis.text.y = element_blank(), 
+            axis.ticks.y = element_blank(),
+            axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
+        )
+
+corr_heatmap <- (corr_heatmap_discovery + corr_heatmap_holdout) +
+    plot_layout(guides = "collect") &
+        theme_pander() &
+        theme(plot.margin = margin(2, 2, 2, 2, "mm"))
+
+ggsave(
+    plot = corr_heatmap, 
+    filename = here("outputs", "figs", "corr_heatmap_aggressiveness_psychopathology.pdf"),
+    width = 8,
+    height = 3.5
+)
