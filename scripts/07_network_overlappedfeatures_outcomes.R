1d40d2e498563c8bbb2af6cdbb36ea5033390c7b
Ting-Yat Wong <yatyat0321@gmail.com>
2023-03-27 17:08:26 -0400

    [feat] add normality transformation

--- scripts/07_network_overlappedfeatures_outcomes.R
+++ scripts/07_network_overlappedfeatures_outcomes.R
@@ -9,6 +9,7 @@ library(bootnet)
 library(pheatmap)
 library(RColorBrewer)
 library(gtsummary)
+library(patchwork)
 
 source(here("src", "R", "utilities.R"))
 
@@ -30,8 +31,7 @@ holdout_df <- read_rds(here("data", "processed", "holdout_dataset.rds"))
 full_df <- discovery_df %>% 
     mutate(split = "Discovery", .before = Age) %>% 
     bind_rows(holdout_df %>% mutate(split = "Holdout", .before = Age)) %>% 
-    left_join(df_cidi, by = "Parti_ID") %>% 
-    select(bpaq_scaled, g_psy, all_of(overlapping_features)) %>% 
+    select(bpaq_tot, g_psy, all_of(overlapping_features)) %>% 
     drop_na() %>% 
     mutate(type = "No Missing")
 
@@ -93,11 +93,62 @@ fa_res <- psych::fa(
     rotate = "oblimin", 
     fm = "minres"
 )
+pdf(file = here("outputs", "figs", "fa_corrplot.pdf"), width = 12, height = 12)
+corrplot::corrplot(
+    fa_res$scores %>% 
+        as_tibble(rownames = NA) %>% 
+        rename_at(vars(starts_with("MR")), ~str_replace(., "MR", "F")) %>% 
+        select(paste0("F", 1:18)) %>% 
+        cor(),
+    col = colorRampPalette(c("navy", "white", "tomato3"))(40),
+    method = "color",
+    mar = rep(0, 4),
+    tl.col = "#1C1C1C",
+    tl.srt = 0,
+    type = c("upper"),
+    diag = FALSE,
+    rect.col = "lightgrey",
+    addCoef.col = "grey40",
+    number.cex = 0.8,
+    number.digits = 2
+)
+dev.off()
+
+auto_transform <- function(x) {
+    bn_obj <- bestNormalize::bestNormalize(x)
+    transformed_x <- predict(bn_obj, x)
+    return(transformed_x)
+}
 
 fa_score_df <- full_df %>% 
-    select(g_psy, bpaq_scaled) %>%
+    select(g_psy, bpaq_tot) %>%
     bind_cols(fa_res$scores) %>% 
-    rename_at(vars(starts_with("MR")), ~str_replace(., "MR", "F"))
+    rename_at(vars(starts_with("MR")), ~str_replace(., "MR", "F")) %>% 
+    mutate_all(.funs = auto_transform) %>% 
+    select(g_psy, bpaq_tot, paste0("F", 1:18))
+
+plot_hist <- function(df, x) {
+    hist_fig <- df %>% 
+        ggplot(aes(x = !!sym(x))) + 
+            geom_histogram(binwidth = 0.5, color = "gray30", fill = "gray30") +
+            labs(y = "") +
+            theme_pander()
+    return(hist_fig)
+}
+
+hist_figs <- list()
+
+for (variable in colnames(fa_score_df)) {
+    hist_figs[[variable]] <- plot_hist(fa_score_df, variable)
+}
+hist_wrap_plot <- wrap_plots(hist_figs, ncol = 5, nrow = 4)
+ggsave(
+    plot = hist_wrap_plot, 
+    filename = here("outputs", "figs", "hist_wrap_plot_transformed.pdf"), 
+    width = 8,
+    height = 6
+)
+
 
 # plot FA loading
 pdf(file = here("outputs", "figs", "fa_heatmap.pdf"), width = 12, height = 14)
@@ -124,9 +175,9 @@ dev.off()
 
 # Symptom Network --------------------------------------------------------------
 network_data       <- list()
-network_data$data  <- fa_score_df %>% select(g_psy, bpaq_scaled, fa_feature_details$label)
+network_data$data  <- fa_score_df %>% select(g_psy, bpaq_tot, fa_feature_details$label)
 network_data$node  <- c("GPsy", "Aggr", fa_feature_details$label)
-network_data$node_detail <- c("General Psychoapthology",
+network_data$node_detail <- c("General Psychopathology",
                               "Proneness to Aggression",
                               fa_feature_details$details)
 network_data$node_col <- c("#B5CAA0", "#B5CAA0", rep("#DAC9A6", n_features))
@@ -240,15 +291,6 @@ g_bs_case <- bootnet(
 )
 write_rds(g_bs_case, here("outputs", "cache", "g_bs_case.rds"))
 
-g_bs_np <- bootnet(
-    g,
-    statistics = c("ExpectedInfluence", "Edge"),
-    nBoots = 1000,
-    nCores = 8,
-    type = "nonparametric"
-)
-write_rds(g_bs_np, here("outputs", "cache", "g_bs_np.rds"))
-
 stability_drop_plot <- plot(g_bs_case, statistics = "ExpectedInfluence") + 
     theme_pander() +
     theme(
@@ -258,10 +300,20 @@ stability_drop_plot <- plot(g_bs_case, statistics = "ExpectedInfluence") +
 stability_drop_plot
 ggsave(
     here("outputs", "figs", "data-discovery_desc-bs_stability_drop.pdf"),
-    stability_plot, 
+    stability_drop_plot, 
     height = 4, 
     width = 8
 )
+
+# check stability of edge
+g_bs_np <- bootnet(
+    g,
+    statistics = c("ExpectedInfluence", "Edge"),
+    nBoots = 1000,
+    nCores = 8,
+    type = "nonparametric"
+)
+write_rds(g_bs_np, here("outputs", "cache", "g_bs_np.rds"))
 
 stability_edge_plot <- plot(g_bs_np,  statistics = "Edge", order = "sample") +
     theme_bw() +
@@ -277,11 +329,5 @@ ggsave(
     width = 8
 )
 plot(g_bs_np, plot = "interval", split0 = TRUE, statistics = "Edge", order = "sample")
-
-
-
-
-
-
 
 
